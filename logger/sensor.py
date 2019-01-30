11a4ab862ea02ff4b162187be4acceef0aa0c907
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2019-01-30 19:22:19 +0100

    Update sensor.py

--- logger/sensor.py
+++ logger/sensor.py
@@ -2,9 +2,4 @@ import Adafruit_DHT
 
 
 def measure():
-    try:
-        return Adafruit_DHT.read_retry(11, 4)
-    except RuntimeError as e:
-        import stringtime
-        with open('log', 'a') as logfile:
-            logfile.write('{} Reading error occured: {} '.format(stringtime.time(), e))
+    return Adafruit_DHT.read_retry(11, 4)
